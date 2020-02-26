e55b8b0a93563bdc75ffebf66fede0e8dbf191b9
krupagaliya <krupagaliya@gmail.com>
2020-02-26 07:23:38 +0530

    file saved and see the shape

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import logging
+
 from flask import Flask, render_template, request
+import os
+from werkzeug.utils import secure_filename
+import cv2
+
+config = {}
+config["IMAGE_UPLOADS"] = "data"
+config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 
 app = Flask(__name__)
 
@@ -7,22 +16,23 @@ app = Flask(__name__)
 def hello_world():
     return render_template("hello.html")
 
+
 @app.route('/input')
 def input():
     return render_template("input.html")
 
+
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    if request.method == 'GET':
-        data = request.files.get('myfile1','')
-
-        print("only data", data)
-        data.save("data", data.filename)
-        print("Image saved")
-
-        print(data.shape)
-    # data = request.form['myfile1']
-    return "Hello"
+    if request.method == 'POST':
+        data = request.files["file1"]
+        filename = secure_filename(data.filename)
+        data.save(os.path.join(config["IMAGE_UPLOADS"], filename))
+        image = cv2.imread(os.path.join(config["IMAGE_UPLOADS"], filename))
+        print(image.shape)
+        res = image.shape
+        # detect_person(image)
+        return str(res)
 
 
 if __name__ == '__main__':
